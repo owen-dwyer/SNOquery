cb75c9848b840b17452527aa70f7c94cc54703cf
owen-dwyer <owen.dwyer98@gmail.com>
2024-02-15 16:32:18 +0000

    categorical recall graph now works

--- src/visualisation/aggregation.R
+++ src/visualisation/aggregation.R
@@ -2,15 +2,19 @@ library(tidyverse)
 library(magrittr)
 library(ggplot2)
 
-retrieved_codelist = read.csv(paste('../codelists/output/omop/query_colorectal_0_snomed_opcs.csv',sep='')) %>% select('opcs') %>% distinct() %>% filter(!grepl("^(Y|Z)", opcs))
+retrieved_codelist = read.csv(paste('../codelists/output/omop/query_colorectal_0_snomed_opcs.csv',sep='')) %>% 
+  select('opcs') %>% 
+  distinct() %>% 
+  filter(!grepl("^(Y|Z)", opcs))
 
 retrieved_codelist$opcs_3char <- substring(retrieved_codelist$opcs, 0, 3)
 retrieved_codelist$opcs_1char <- substring(retrieved_codelist$opcs, 0, 1)
 
 
-ggplot(data = data.frame(table(retrieved_codelist$opcs_3char)) ) + 
+opcs_3char_count <- data.frame(table(retrieved_codelist$opcs_3char))
+ggplot(data = opcs_3char_count ) + 
   geom_col( aes(x=Var1, y=Freq))
 
-
-ggplot(data = data.frame(table(retrieved_codelist$opcs_1char)) ) + 
+opcs_1char_count <- data.frame(table(retrieved_codelist$opcs_1char))
+ggplot(data = opcs_1char_count) + 
   geom_col( aes(x=Var1, y=Freq))
